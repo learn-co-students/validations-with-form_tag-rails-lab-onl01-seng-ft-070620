dc1488ea5a1c44bfbbe40320903978a8248871eb
Ethan <eleetyson@gmail.com>
2020-09-13 11:24:20 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+  # validates name isn't blank
+  # email is unique
+  # phone number is exactly 10 digits
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  # validates title isn't blank
+  # content is 100+ chars
+  # category is either "Fiction" or "Non-Fiction"
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
